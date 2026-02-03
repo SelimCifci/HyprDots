26a6a72228770c6a8b2476f9df99385e850b55ce
SelimCifci <cifciselimhasan@gmail.com>
2026-02-03 20:33:36 +0100

    Added yay installation to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git ..
+cd ../yay
+makepkg -si
+
 yay -S hyprland dunst kitty tofi waybar wlogout sddm tinct-bin python-pywal16 sddm-theme-corners-git uwsm swww nwg-look qt5ct qt6ct dolphin \
 ttf-jetbrains-mono-nerd hyprlock firefox pywalfox visual-studio-code-bin vim zsh \
 adw-gtk-theme pavucontrol cliphist grimblast hyprpicker bibata-cursor-theme curl
