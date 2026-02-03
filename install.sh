82436f29da7634ad2c1651d9a5c7cdc9aa361f89
SelimCifci <cifciselimhasan@gmail.com>
2026-02-03 21:00:16 +0100

    .

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
 makepkg -si
 
 yay -S hyprland dunst kitty tofi waybar wlogout sddm tinct-bin python-pywal16 sddm-theme-corners-git uwsm swww nwg-look qt5ct qt6ct dolphin ttf-jetbrains-mono-nerd hyprlock firefox pywalfox visual-studio-code-bin vim zsh adw-gtk-theme pavucontrol cliphist grimblast hyprpicker bibata-cursor-theme curl
 
-systemctl enable sddm.service
+sudo systemctl enable sddm.service
 
 cd ~/HyprDots
 cp -r * ~/
-cp -r ~/sddm/* /usr/share/sddm/themes/corners/
+sudo cp -r ~/sddm/* /usr/share/sddm/themes/corners/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
